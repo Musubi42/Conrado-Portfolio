b6e2929ad6ac4fcf16dd62b4b077be2c994d0c13
Raphael THIBAUT <raphael.dev@protonmail.com>
2022-03-06 21:29:03 +0100

    On ne garde que le front

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Header from "./Components/header/Header";
+import Footer from "./Components/footer/Footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <p>test</p>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/header/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./header.css"
+import logoConrado from '../../images/Logo_Conrado.svg'
+
+export default function header() {
+  return (
+    <div className="header">
+      <div className="accueil">
+        <img className="logo" src={logoConrado} alt="Logo Conrado" />
+      </div>
+      <div className="navigation">
+        <ul className="list">
+          <li className="item">ACCUEIL</li>
+          <li className="item">LOGOS</li>
+          <li className="item">MOCKUPS</li>
+          <li className="item">PAGE WEB</li>
+          <li className="item">A PROPOS</li>
+          <li className="item">CONTACT</li>
+        </ul>
+      </div>
+    </div>
+  )
+}
