a4bab41096376d6aafebd801b391fb767753c215
Musubi42 <raphael.dev@protonmail.com>
2022-03-07 00:42:55 +0100

    Modification emplacement watermark

--- src/Components/footer/Footer.jsx
+++ src/Components/footer/Footer.jsx
@@ -5,10 +5,10 @@ export default function footer() {
   return (
     <div className="footer">
       <div className="footerGauche">
-          <span className="footerWatermark">CONRAD DESIGN 2022 TOUS DROIT RESERVES</span>
       </div>
       <div className="footerMilieu">
         <ul className="footerList">
+          <li className="footerItem" style={{cursor: "unset"}}>CONRAD DESIGN 2022 TOUS DROIT RESERVES</li>
           <li className="footerItem"><a href="/">MENTIONS LEGALES</a></li>
           <li className="footerItem"><a href="/">FAQ</a></li>
           <li className="footerItem"><a href="/">POLITIQUE DE CONFIDENTIALITE</a></li>
