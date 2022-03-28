8f7c36ce279581e00cd3fdb4f31265809ba98855
Musubi42 <raphael.dev@protonmail.com>
2022-03-29 01:32:33 +0200

    Modification lien Asset

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom"
 import "./header.css"
-import logoConrado from '../../images/Logo_Conrado.svg'
+import logoConrado from '../../Assets/Logo_Conrado.svg'
 
 export default function header() {
   return (
