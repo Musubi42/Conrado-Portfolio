60545e726c68a81058509d34f26c26be58adc824
Musubi42 <raphael.dev@protonmail.com>
2022-03-29 01:32:54 +0200

    Evol Page Home

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import "./home.css"
+import background from './../../Assets/Costard_Cravatte.png'
 
 export default function Home() {
   return (
-    <div>Accueil</div>
+  <div className="homeBGI">
+    {/* <div className="homeBGI"></div> */}
+    <div className="quote">UN TRAVAIL QUALITATIF DOIT PRIMER SUR UN TRAVAIL ORIGINAL</div>
+    <div className="text">Dans le monde du design beaucoup essaie d'être original à tout prix quitte à proposer des rendus de moins bonnes qualité. Ce n'est pas mon cas. C'est avec cette conviction que je mets mes compétences en pratique. Ce dont vous avez besoin, ce que j epeux vous apporter , faites-vous une brève opinion de ce que vous pouvez obtenir en travaillant à mes côtés à l'aide de mon portfolio.</div>
+    <button className="contact">ME CONTACTER</button>
+  </div>
   )
 }
