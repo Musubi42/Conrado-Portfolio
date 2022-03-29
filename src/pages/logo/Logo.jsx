cae7646a6085c9d1287f7d42a99c5af87cdb4923
Musubi42 <raphael.dev@protonmail.com>
2022-03-29 21:22:53 +0200

    Ajout du contenu - grid

--- src/pages/logo/Logo.jsx
+++ src/pages/logo/Logo.jsx
@@ -3,6 +3,19 @@ import "./logo.css"
 
 export default function Logo() {
   return (
-    <div className="logo">Logo Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatem obcaecati accusantium, omnis labore excepturi nam vero facere qui. Aperiam illum mollitia sapiente voluptas vel sed quae autem et inventore, nobis magni beatae labore unde consectetur atque asperiores modi ut nulla quam animi, placeat ullam velit quisquam dolorum. Quasi, quo a consectetur dolores dignissimos modi corrupti amet ad eveniet voluptatibus neque nihil cum soluta aperiam dolor iste sapiente nesciunt deserunt nulla deleniti voluptatem nisi quam voluptates! At velit vel explicabo minima quasi consequatur illo voluptatibus ab! Libero cumque eum nisi, autem praesentium, optio voluptates recusandae magnam officia numquam labore illum minima.</div>
+    <div className="logosMain">
+      <h1 className="logosTitle">LOGOS</h1>
+      <div className="logosContainer">
+        {/* Venir ajouter les logos à la suite */}
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+        <div className="logosItems"></div>
+      </div>
+    </div>
   )
 }
