6ccb6dc25299c58b984d5d4245636c6d6dbfba6c
Musubi42 <raphael.dev@protonmail.com>
2022-04-01 23:15:50 +0200

    Ajout import REACT

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Components/header/Header";
 import Footer from "./Components/footer/Footer";
