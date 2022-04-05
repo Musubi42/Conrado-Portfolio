0825653f11542bebbb88eb483576655f5b06f6bf
Musubi42 <raphael.dev@protonmail.com>
2022-04-05 23:44:21 +0200

    rennomage propriétés CSS

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from "react-router-dom"
+import { NavLink } from 'react-router-dom'
 import "./header.css"
 import logoConrado from '../../Assets/Logo_Conrado.svg'
 
@@ -12,13 +13,14 @@ export default function header() {
           <span className="headerLogoName">CONRAD</span><span className="headerLogoName">EDISON</span>
         </div>
       </div>
-        <ul className="list">
-          <li className="item"><Link className="link" to="/accueil">ACCUEIL</Link></li>
-          <li className="item"><Link className="link" to="/logo">LOGO</Link></li>
-          <li className="item"><Link className="link" to="/mockup">MOCKUPS</Link></li>
-          <li className="item"><Link className="link" to="/page_web">PAGE WEB</Link></li>
-          <li className="item"><Link className="link" to="/a_propos">A PROPOS</Link></li>
-          <li className="item"><Link className="link" to="/contact">CONTACT</Link></li>
+        <ul className="headerList">
+          <li className="headerItem"><NavLink className="link" to="logo" >Logo</NavLink></li>
+          <li className="headerItem"><Link className="link" to="/accueil">ACCUEIL</Link></li>
+          <li className="headerItem"><Link className="link" to="/logo">LOGO</Link></li>
+          <li className="headerItem"><Link className="link" to="/mockup">MOCKUPS</Link></li>
+          <li className="headerItem"><Link className="link" to="/page_web">PAGE WEB</Link></li>
+          <li className="headerItem"><Link className="link" to="/a_propos">A PROPOS</Link></li>
+          <li className="headerItem"><Link className="link" to="/contact">CONTACT</Link></li>
         </ul>
     </div>
   )
