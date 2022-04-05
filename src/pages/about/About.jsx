d8c477687a46faf359ddb0dbe5f1582c65f0376e
Musubi42 <raphael.dev@protonmail.com>
2022-04-05 23:47:24 +0200

    Suppression élément non utilisé

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 import "./about.css"
 import imageConrado from '../../Assets/Ricardo_Milanos.png'
 
@@ -11,6 +11,7 @@ export default function About() {
         <div className="aboutPresentation">
           <p className="aboutTextPrensetation">Mentor, Consultant et Coach sont les compétences que j'ai durement acquises durant plusieurs années de recherche, de questionnement et de mise en pratique.</p>
           <p className="aboutTextPrensetation">Après avoir réalisé plusieurs livres et formations dans le domaine du développement pernneil, je suis à même de répondre à vos besoins le splus profonds et de faire que vos souhaits deviennet réalité, à condition que vous décidiez de prendre votre vie en main.</p>
+          {/* TODO : Faire le link vers la page contact */}
           <p className="aboutTextPrensetation">Si vous souhaitez devenir maître de votre vie au lieu de la subir, prennez contact avez nous ICI.</p>
         </div>
         <img className="aboutImagePrensetation" src={imageConrado} alt="de Conrado"/>
