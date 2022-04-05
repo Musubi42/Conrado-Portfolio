2e909c0c956e0b033eb853d2dcf24a68b8ba1674
Musubi42 <raphael.dev@protonmail.com>
2022-04-05 23:45:31 +0200

    Création WebPage

--- src/pages/webPage/WebPage.jsx
+++ src/pages/webPage/WebPage.jsx
@@ -3,6 +3,12 @@ import "./webPage.css"
 
 export default function WebPage() {
   return (
-    <div>WebPage</div>
+    <div className="webPageMain">
+      <h1 className="webPageTitle">PAGE WEB</h1>
+      <div className="webPageContainer">
+        <div className="webPageItems"></div>
+        <div className="webPageItems"></div>
+      </div>
+    </div>
   )
 }
