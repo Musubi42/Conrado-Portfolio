8e62db8697a8ddc0b23d272afadc6d9be7e7c6ac
Musubi42 <raphael.dev@protonmail.com>
2022-04-07 12:08:34 +0200

    Création contact

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,7 +1,64 @@
-import React from 'react'
+import React from "react";
+import "./contact.css";
+import logoConrado from "../../Assets/Logo_Conrado.svg";
 
 export default function Contact() {
   return (
-    <div>Contact</div>
-  )
+    <div className="contactMain">
+      <div className="contactDivContact">
+        <h1 className="contactTitleContact">CONTACTEZ MOI</h1>
+        <div className="contactDivForm">
+          <img
+            src={logoConrado}
+            alt="Logo de la marque conrad edison"
+            className="contactLogoConrado"
+          />
+          <form action="submit" className="contactForm">
+            <input
+              className="contactInput"
+              type="text"
+              placeholder="Nom/Prénom"
+            />
+            <input className="contactInput" type="email" placeholder="Mail" />
+            <input
+              className="contactInput"
+              type="tel"
+              placeholder="Téléphone"
+            />
+            <input className="contactInput" type="text" placeholder="Objet" />
+            <input
+              className="contactInput"
+              type="text"
+              placeholder="Description"
+            />
+          </form>
+        </div>
+        <button className="contactButtonContact">SOUMETTRE</button>
+      </div>
+      {/* TODO : Faire l'inté des img avec CSS et en HTML pour tester */}
+      <div className="contactNetworks">
+        <h1 className="contactNetworkTitle">CONTACT</h1>
+        <div className="contactNetwork">
+          <div className="contactNetworkDiv">
+            <button className="contactNetworkButton"></button>
+            <span className="contactNetworkNameButton"></span>
+          </div>
+          <div className="contactNetworkDiv">
+            <button className="contactNetworkButton"></button>
+            <span className="contactNameNetworkButton"></span>
+          </div>
+
+          <div className="contactNetworkDiv">
+            <button className="contactNetworkButton"></button>
+            <span className="contactNetworkNameButton"></span>
+          </div>
+
+          <div className="contactNetworkDiv">
+            <button className="contactNetworkButton"></button>
+            <span className="contactNetworkNameButton"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
