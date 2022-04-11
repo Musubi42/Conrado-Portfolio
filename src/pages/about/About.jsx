ae4b5eae0594148f90098c15533f3fdbd9ee9e37
Musubi42 <raphael.dev@protonmail.com>
2022-04-11 23:57:42 +0200

    modification path logo

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -1,7 +1,7 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./about.css"
-import imageConrado from '../../Assets/Ricardo_Milanos.png'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./about.css";
+import imageConrado from "../../Assets/ricardo_milanos.png";
 
 export default function About() {
   return (
@@ -9,13 +9,29 @@ export default function About() {
       <div className="aboutTitle">A PROPOS</div>
       <div className="aboutContainer">
         <div className="aboutPresentation">
-          <p className="aboutTextPrensetation">Mentor, Consultant et Coach sont les compétences que j'ai durement acquises durant plusieurs années de recherche, de questionnement et de mise en pratique.</p>
-          <p className="aboutTextPrensetation">Après avoir réalisé plusieurs livres et formations dans le domaine du développement pernneil, je suis à même de répondre à vos besoins le splus profonds et de faire que vos souhaits deviennet réalité, à condition que vous décidiez de prendre votre vie en main.</p>
+          <p className="aboutTextPrensetation">
+            Mentor, Consultant et Coach sont les compétences que j'ai durement
+            acquises durant plusieurs années de recherche, de questionnement et
+            de mise en pratique.
+          </p>
+          <p className="aboutTextPrensetation">
+            Après avoir réalisé plusieurs livres et formations dans le domaine
+            du développement pernneil, je suis à même de répondre à vos besoins
+            le splus profonds et de faire que vos souhaits deviennet réalité, à
+            condition que vous décidiez de prendre votre vie en main.
+          </p>
           {/* TODO : Faire le link vers la page contact */}
-          <p className="aboutTextPrensetation">Si vous souhaitez devenir maître de votre vie au lieu de la subir, prennez contact avez nous <Link to="/contact">ICI</Link>.</p>
+          <p className="aboutTextPrensetation">
+            Si vous souhaitez devenir maître de votre vie au lieu de la subir,
+            prennez contact avez nous <Link to="/contact">ICI</Link>.
+          </p>
         </div>
-        <img className="aboutImagePrensetation" src={imageConrado} alt="de Conrado"/>
+        <img
+          className="aboutImagePrensetation"
+          src={imageConrado}
+          alt="de Conrado"
+        />
       </div>
     </div>
-  )
+  );
 }
