313332c4ff456f09a828d382d5b740278223b33b
Musubi42 <raphael.dev@protonmail.com>
2022-04-27 22:24:34 +0200

    Page en maintenance

--- src/pages/mockup/Mockup.jsx
+++ src/pages/mockup/Mockup.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import "./mockup.css"
+import React from "react";
+import "./mockup.css";
 
 export default function Mockup() {
   return (
     <div className="mockupMain">
       <h1 className="mockupTitle">MOCKUPS</h1>
       <div className="mockupContainer">
-        <div className="mockupItems">je suis</div>
-        <div className="mockupItems">je suis</div>
-        <div className="mockupItems">je suis</div>
+        <div className="mockupMaintenance">
+          <p>EN MAINTENANCE</p>
+        </div>
       </div>
     </div>
-  )
+  );
 }
