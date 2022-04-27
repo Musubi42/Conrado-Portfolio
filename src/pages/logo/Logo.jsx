758542804e513c688d4d6ce10f280be3f834206c
Musubi42 <raphael.dev@protonmail.com>
2022-04-27 22:24:55 +0200

    Ajout des logos

--- src/pages/logo/Logo.jsx
+++ src/pages/logo/Logo.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import "./logo.css";
 // import logoConrado from "../../Assets/logo_conrado.svg";
-import logoRedPill from "../../Assets/images/Logos/logo_red_pill.jpg";
+import logoLes3Caps from "../../Assets/images/Logos/logo_les_3_caps.jpg";
 import logoRoswell from "../../Assets/images/Logos/logo_roswell.jpg";
+import logoGiorno2077 from "../../Assets/images/Logos/logo_giorno_2077.jpg";
 import logoLorenzo from "../../Assets/images/Logos/logo_lorenzo_scimone.jpg";
-import logoBaki from "../../Assets/images/Logos/logo_baki_fond_noir.jpg";
-import logoJPCarrelage from "../../Assets/images/Logos/logo_jp_carrelage.jpg";
+import logoFondationMagister from "../../Assets/images/Logos/logo_fondation_magister.jpg";
+import logoRedPill from "../../Assets/images/Logos/logo_red_pill.jpg";
+import logoBaki from "../../Assets/images/Logos/logo_baki.jpg";
+import logoJeromePoisson from "../../Assets/images/Logos/logo_jerome_poisson.jpg";
 
 export default function Logo() {
   return (
@@ -13,11 +16,14 @@ export default function Logo() {
       <h1 className="logoTitle">logo</h1>
       <div className="logoContainer">
         {/* Venir ajouter les logo à la suite */}
-        <img className="logoItems" src={logoRedPill} alt="" />
+        <img className="logoItems" src={logoLes3Caps} alt="" />
         <img className="logoItems" src={logoRoswell} alt="" />
+        <img className="logoItems" src={logoGiorno2077} alt="" />
         <img className="logoItems" src={logoLorenzo} alt="" />
+        <img className="logoItems" src={logoFondationMagister} alt="" />
         <img className="logoItems" src={logoBaki} alt="" />
-        <img className="logoItems" src={logoJPCarrelage} alt="" />
+        <img className="logoItems" src={logoRedPill} alt="" />
+        <img className="logoItems" src={logoJeromePoisson} alt="" />
         {/* <img className="logoItems" src="" alt="" />
         <img className="logoItems" src="" alt="" />
         <img className="logoItems" src="" alt="" /> */}
