a634ad1433c193ac628d72e0d0ddb1e1ad5bcc13
Musubi42 <raphael.dev@protonmail.com>
2022-04-27 19:53:25 +0200

    Amélioration CSS

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import "./home.css"
+import React from "react";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 export default function Home() {
   return (
-  <div className="homeBGI">
-    {/* <div className="homeBGI"></div> */}
-    <div className="quote">UN TRAVAIL QUALITATIF DOIT PRIMER SUR UN TRAVAIL ORIGINAL</div>
-    <div className="text">Dans le monde du design beaucoup essaie d'être original à tout prix quitte à proposer des rendus de moins bonnes qualité. Ce n'est pas mon cas. C'est avec cette conviction que je mets mes compétences en pratique. Ce dont vous avez besoin, ce que j epeux vous apporter , faites-vous une brève opinion de ce que vous pouvez obtenir en travaillant à mes côtés à l'aide de mon portfolio.</div>
-    {/* TODO : Faire un Link vers la page de formulaire de contact */}
-    <button className="contact"><Link className="link" to="/contact">ME CONTACTER</Link></button>
-  </div>
-  )
+    <div className="homeBGI">
+      {/* <div className="homeBGI"></div> */}
+      <div className="homeQuote">
+        "UN TRAVAIL QUALITATIF DOIT PRIMER SUR UN TRAVAIL ORIGINAL"
+      </div>
+      <div className="homeText">
+        Dans le monde du design beaucoup essaie d'être original à tout prix
+        quitte à proposer des contenus de moins bonnes qualité. Ce n'est pas
+        notre cas. Chez Conrad Edison Agency nous mettons en avant la qualité de
+        nos services. Ce dont vous avez besoin, forgez-votre opinion de ce que
+        nous pouvons vous garantir à travers l'étendu de notre site.
+      </div>
+      {/* TODO : Faire un Link vers la page de formulaire de contact */}
+      <button className="homeContact">
+        <Link className="link" to="/contact">
+          NOUS CONTACTER
+        </Link>
+      </button>
+    </div>
+  );
 }
