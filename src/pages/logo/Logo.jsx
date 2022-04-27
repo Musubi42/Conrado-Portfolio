2e4bf65638a7a49fab7b066ddd9ca0762bfeeb71
Musubi42 <raphael.dev@protonmail.com>
2022-04-27 19:53:38 +0200

    Ajout Logos

--- src/pages/logo/Logo.jsx
+++ src/pages/logo/Logo.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import "./logo.css"
+import React from "react";
+import "./logo.css";
+// import logoConrado from "../../Assets/logo_conrado.svg";
+import logoRedPill from "../../Assets/images/Logos/logo_red_pill.jpg";
+import logoRoswell from "../../Assets/images/Logos/logo_roswell.jpg";
+import logoLorenzo from "../../Assets/images/Logos/logo_lorenzo_scimone.jpg";
+import logoBaki from "../../Assets/images/Logos/logo_baki_fond_noir.jpg";
+import logoJPCarrelage from "../../Assets/images/Logos/logo_jp_carrelage.jpg";
 
 export default function Logo() {
   return (
@@ -7,15 +13,15 @@ export default function Logo() {
       <h1 className="logoTitle">logo</h1>
       <div className="logoContainer">
         {/* Venir ajouter les logo à la suite */}
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
-        <div className="logoItems"></div>
+        <img className="logoItems" src={logoRedPill} alt="" />
+        <img className="logoItems" src={logoRoswell} alt="" />
+        <img className="logoItems" src={logoLorenzo} alt="" />
+        <img className="logoItems" src={logoBaki} alt="" />
+        <img className="logoItems" src={logoJPCarrelage} alt="" />
+        {/* <img className="logoItems" src="" alt="" />
+        <img className="logoItems" src="" alt="" />
+        <img className="logoItems" src="" alt="" /> */}
       </div>
     </div>
-  )
+  );
 }
