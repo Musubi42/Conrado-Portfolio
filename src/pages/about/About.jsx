edaacfbb50323cee3aace4a1325814151ebf2f3c
Musubi42 <raphael.dev@protonmail.com>
2022-05-09 16:48:36 +0200

    Ajout texte about

--- src/pages/about/About.jsx
+++ src/pages/about/About.jsx
@@ -13,6 +13,17 @@ export default function About() {
           <p className="aboutTextPrensetation"></p>
           {/* TODO : Faire le link vers la page contact */}
           <p className="aboutTextPrensetation">
+            Notre équipe est spécialisée dans le personnal branding
+            d’influenceurs motivation, notre objectif est de créer une identité
+            forte et unique et ainsi d’augmenter les rendements de nos
+            partenaires de façon pérenne.
+            <br /> <br />
+            Consultez-nous, nous ferons en sorte de mettre un doigt sur ce qui
+            freine votre ascension professionnelle et nous vous guiderons afin
+            de capter au mieux les besoins de votre audience.
+            <br />
+            <br />
+            Au plaisir de travailler à vos côtés !
             {/* prennez contact avec nous <Link to="/contact">ICI</Link>. */}
           </p>
         </div>
