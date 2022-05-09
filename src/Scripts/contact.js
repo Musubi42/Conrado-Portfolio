9b8d685bfbe17fa295fbf052daa6f7349b56777d
Musubi42 <raphael.dev@protonmail.com>
2022-05-10 00:52:38 +0200

    Fix copyText

--- src/Scripts/contact.js
+++ src/Scripts/contact.js
@@ -7,7 +7,6 @@ window.onload = function () {
     mutations.forEach(function (mutation) {
       if (oldHref !== document.location.href) {
         oldHref = document.location.href;
-        console.log("changed");
 
         const networkButton = {
           BUTTON_INSTAGRAM: document.getElementById("buttonInstagram"),
@@ -68,8 +67,11 @@ window.onload = function () {
         networkReferences.REFERENCE_INSTAGRAM.addEventListener(
           "click",
           function (el) {
+            console.log(networkReferences.REFERENCE_INSTAGRAM);
             navigator.clipboard
-              .writeText(el.path[1].childNodes[0].textContent)
+              .writeText(
+                networkReferences.REFERENCE_INSTAGRAM.firstChild.textContent
+              )
               .then(
                 function () {
                   displayCopied(el);
@@ -85,7 +87,9 @@ window.onload = function () {
           "click",
           function (el) {
             navigator.clipboard
-              .writeText(el.path[1].childNodes[0].textContent)
+              .writeText(
+                networkReferences.REFERENCE_INSTAGRAM.firstChild.textContent
+              )
               .then(
                 function () {
                   displayCopied(el);
@@ -101,7 +105,9 @@ window.onload = function () {
           "click",
           function (el) {
             navigator.clipboard
-              .writeText(el.path[1].childNodes[0].textContent)
+              .writeText(
+                networkReferences.REFERENCE_INSTAGRAM.firstChild.textContent
+              )
               .then(
                 function () {
                   displayCopied(el);
@@ -117,7 +123,9 @@ window.onload = function () {
           "click",
           function (el) {
             navigator.clipboard
-              .writeText(el.path[1].childNodes[0].textContent)
+              .writeText(
+                networkReferences.REFERENCE_INSTAGRAM.firstChild.textContent
+              )
               .then(
                 function () {
                   displayCopied(el);
@@ -139,214 +147,3 @@ window.onload = function () {
 
   observer.observe(bodyList, config);
 };
-
-document.addEventListener("DOMContentLoaded", () => {
-  const networkButton = {
-    BUTTON_INSTAGRAM: document.getElementById("buttonInstagram"),
-    BUTTON_LINKEDIN: document.getElementById("buttonLinkedin"),
-    BUTTON_MAIL: document.getElementById("buttonMail"),
-  };
-
-  const networkReferences = {
-    REFERENCE_INSTAGRAM: document.getElementById("referenceInstagram"),
-    REFERENCE_LINKEDIN: document.getElementById("referenceLinkedin"),
-    REFERENCE_MAIL: document.getElementById("referenceMail"),
-    REFERENCE_TELE: document.getElementById("referenceTel"),
-  };
-
-  document
-    .getElementById("buttonInstagram")
-    .addEventListener("click", function () {
-      window.open("https://www.instagram.com/julien.boulet/", "_blank");
-    });
-
-  networkButton.BUTTON_LINKEDIN.addEventListener("click", function () {
-    window.open(
-      "https://www.linkedin.com/in/julien-boulet-a8a8b818b/",
-      "_blank"
-    );
-  });
-
-  networkButton.BUTTON_MAIL.addEventListener("click", function () {
-    window.open("mailto:", "contact@conrad-edison.fr");
-  });
-
-  // Fonction pour afficher masquer le copier/actif
-  const toggleClass = function (ARRAY_LOGOS) {
-    // Logo Copier
-    ARRAY_LOGOS[1].classList.add("displayActive");
-    ARRAY_LOGOS[1].classList.remove("displayNone");
-    // Logo copié
-    ARRAY_LOGOS[0].classList.remove("displayActive");
-    ARRAY_LOGOS[0].classList.add("displayNone");
-  };
-
-  const displayCopied = function (element) {
-    const ARRAY_LOGOS = element.target.parentElement.children;
-    if (ARRAY_LOGOS[1].classList[2] === "displayActive") {
-      // Logo Copier
-      ARRAY_LOGOS[1].classList.remove("displayActive");
-      ARRAY_LOGOS[1].classList.add("displayNone");
-      // Logo copié
-      ARRAY_LOGOS[0].classList.add("displayActive");
-      ARRAY_LOGOS[0].classList.remove("displayNone");
-      window.setTimeout(function () {
-        toggleClass(ARRAY_LOGOS);
-      }, 1000);
-    }
-  };
-
-  // When user click on the button, copy the text
-  networkReferences.REFERENCE_INSTAGRAM.addEventListener(
-    "click",
-    function (el) {
-      navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-        function () {
-          displayCopied(el);
-        },
-        function (err) {
-          console.error("Could not copy text: ", err);
-        }
-      );
-    }
-  );
-
-  networkReferences.REFERENCE_LINKEDIN.addEventListener("click", function (el) {
-    navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-      function () {
-        displayCopied(el);
-      },
-      function (err) {
-        console.error("Could not copy text: ", err);
-      }
-    );
-  });
-
-  networkReferences.REFERENCE_MAIL.addEventListener("click", function (el) {
-    navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-      function () {
-        displayCopied(el);
-      },
-      function (err) {
-        console.error("Could not copy text: ", err);
-      }
-    );
-  });
-
-  networkReferences.REFERENCE_TELE.addEventListener("click", function (el) {
-    navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-      function () {
-        displayCopied(el);
-      },
-      function (err) {
-        console.error("Could not copy text: ", err);
-      }
-    );
-  });
-});
-
-// if (window.location.href === "https://conrad-edison/contact") {
-//   console.log("first");
-//   const networkButton = {
-//     BUTTON_INSTAGRAM: document.getElementById("buttonInstagram"),
-//     BUTTON_LINKEDIN: document.getElementById("buttonLinkedin"),
-//     BUTTON_MAIL: document.getElementById("buttonMail"),
-//   };
-
-//   const networkReferences = {
-//     REFERENCE_INSTAGRAM: document.getElementById("referenceInstagram"),
-//     REFERENCE_LINKEDIN: document.getElementById("referenceLinkedin"),
-//     REFERENCE_MAIL: document.getElementById("referenceMail"),
-//     REFERENCE_TELE: document.getElementById("referenceTel"),
-//   };
-
-//   document
-//     .getElementById("buttonInstagram")
-//     .addEventListener("click", function () {
-//       window.open("https://www.instagram.com/julien.boulet/", "_blank");
-//     });
-
-//   networkButton.BUTTON_LINKEDIN.addEventListener("click", function () {
-//     window.open(
-//       "https://www.linkedin.com/in/julien-boulet-a8a8b818b/",
-//       "_blank"
-//     );
-//   });
-
-//   networkButton.BUTTON_MAIL.addEventListener("click", function () {
-//     window.open("mailto:", "contact@conrad-edison.fr");
-//   });
-
-//   // Fonction pour afficher masquer le copier/actif
-//   const toggleClass = function (ARRAY_LOGOS) {
-//     // Logo Copier
-//     ARRAY_LOGOS[1].classList.add("displayActive");
-//     ARRAY_LOGOS[1].classList.remove("displayNone");
-//     // Logo copié
-//     ARRAY_LOGOS[0].classList.remove("displayActive");
-//     ARRAY_LOGOS[0].classList.add("displayNone");
-//   };
-
-//   const displayCopied = function (element) {
-//     const ARRAY_LOGOS = element.target.parentElement.children;
-//     if (ARRAY_LOGOS[1].classList[2] === "displayActive") {
-//       // Logo Copier
-//       ARRAY_LOGOS[1].classList.remove("displayActive");
-//       ARRAY_LOGOS[1].classList.add("displayNone");
-//       // Logo copié
-//       ARRAY_LOGOS[0].classList.add("displayActive");
-//       ARRAY_LOGOS[0].classList.remove("displayNone");
-//       window.setTimeout(function () {
-//         toggleClass(ARRAY_LOGOS);
-//       }, 1000);
-//     }
-//   };
-
-//   // When user click on the button, copy the text
-//   networkReferences.REFERENCE_INSTAGRAM.addEventListener(
-//     "click",
-//     function (el) {
-//       navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-//         function () {
-//           displayCopied(el);
-//         },
-//         function (err) {
-//           console.error("Could not copy text: ", err);
-//         }
-//       );
-//     }
-//   );
-
-//   networkReferences.REFERENCE_LINKEDIN.addEventListener("click", function (el) {
-//     navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-//       function () {
-//         displayCopied(el);
-//       },
-//       function (err) {
-//         console.error("Could not copy text: ", err);
-//       }
-//     );
-//   });
-
-//   networkReferences.REFERENCE_MAIL.addEventListener("click", function (el) {
-//     navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-//       function () {
-//         displayCopied(el);
-//       },
-//       function (err) {
-//         console.error("Could not copy text: ", err);
-//       }
-//     );
-//   });
-
-//   networkReferences.REFERENCE_TELE.addEventListener("click", function (el) {
-//     navigator.clipboard.writeText(el.path[1].childNodes[0].textContent).then(
-//       function () {
-//         displayCopied(el);
-//       },
-//       function (err) {
-//         console.error("Could not copy text: ", err);
-//       }
-//     );
-//   });
-// }
