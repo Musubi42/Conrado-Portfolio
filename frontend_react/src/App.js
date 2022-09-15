ac2094a7ff0424ac8b2fcfc500da7e192c774334
musubi42 <raphael.dev@protonmail.com>
2022-09-15 14:29:37 +0200

    Add some containers

---
+++ frontend_react/src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+import Header from './container/Header'
+import Footer from './container/Footer'
+import About from './container/About'
+import Home from './container/Home'
+import Branding from './container/Branding'
+import Contact from '
+
+
+
+
+const App = () => { 
+  return (
+    <h1>App</h1>
+  )
+}
+
+export default App
