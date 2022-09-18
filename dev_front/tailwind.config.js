238bd526ddbe54334ab17efde5d937f348b746a2
musubi42 <raphael.dev@protonmail.com>
2022-09-18 16:45:08 +0200

    Ajout custom padding

--- dev_front/tailwind.config.js
+++ dev_front/tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
       'primary': '#11203A',
       'primary-hover': '#FFCA0C',
       'secondary': '#FFCA0C'
+      },
+      padding: {
+        '1/2': '50%',
+        'full': '100%'
       }
     },
   },
