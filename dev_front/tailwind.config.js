7d23242a7c996ad5e0b30c7d7dd2f96b324ba042
musubi42 <raphael.dev@protonmail.com>
2022-11-01 23:32:16 +0100

    Add Tailwind config

--- dev_front/tailwind.config.js
+++ dev_front/tailwind.config.js
@@ -16,6 +16,28 @@ module.exports = {
         'formContact': "url(Assets/BGFormContact.webp)",
         'AlfonZoAcademyMockup': "url(Assets/AlfonZoAcademyMockup.webp)",
         'RoswellMockup': "url(Assets/RoswellMockup.webp)",
+        'ppGiorno': "url(https://yt3.ggpht.com/ErkCTFR662z_qtSuExHndLjQKMa8hyjdFJj7o8NM5GtElDTTYGPKo6teV8xyMJcvQEIVX89Trpk=s176-c-k-c0x00ffffff-no-rj)",
+        'ppFondationMagister': "url(https://yt3.ggpht.com/ytc/AMLnZu9sO6JQAdl3Hf-CpbuxZA_1QfJ6yLcr16OL_9ru=s176-c-k-c0x00ffffff-no-rj)",
+      },
+      height: {
+        'page': 'calc(100vh - 178px)',
+        'logo': '450px',
+        'logoTablet': '280px',
+        'logoMobile': '180px',
+      },
+      width: {
+        'quote': '430px',
+      },
+      borderWidth: {
+        '6': '6px',
+      },
+      fontSize: {
+        'quote': '34px',
+      },
+      fontFamily: {
+        'montserrat': ['Montserrat', 'sans-serif'],
+        'aclo': ['Aclonica', 'sans-serif'],
+        'script': ['Aguafina Script', 'cursive'],
       }
     },
   },
